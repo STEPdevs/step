82552586c9c98c4da0aeff2218ccfecc1cbf0060
SandeepK <sinha.sandeep01@gmail.com>
2014-01-23 00:52:13 +0530

    Added jquery images and css file for calander

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-    $('#date').datepicker({
+
+    function isAnyFieldEmpty(fields){
+        for(i=0;i<fields.length-1;i++){
+            if (fields[i]==="") return true;
+        }
+        return false;
+    }
+
+    $('#dob').datepicker({
         minDate: new Date("/11/12/12"), maxDate: new Date("/12/12/12")
     });
+
+    $('#submit').click(function(){
+        var allFields=[$('#name').val(),$('#email').val(),$('#dob').val(),$('#address').val(),$('#city').val(),$('#number').val()];
+
+        if(!isAnyFieldEmpty(allFields)) {
+            alert("Successfully completed");
+        }
+
+    })
 
     $("#user_email").change(function(){
         var email= $(this).val();
@@ -13,16 +30,19 @@ $(document).ready(function(){
         .success(function(data){
                 emailExist = "<div id=user-validation> This User is already taken </div>";
                 emailNotExist = "<div id=user-validation> <img src='yes.jpeg' width='16' height='10' alt='Edit Entry' /></div>" ;
-                if(data == "false") {
-                    $('#user-validation').html(emailExist).css("color","red");
-                    $('#user_email').css('border-color','red');
+                if(email != '')
+                {
+                    if(data == "false") {
+                        $('#user-validation').html(emailExist).css("color","red");
+                        $('#user_email').css('border-color','red');
 
-                }
-                else{
-                    $('#user-validation').html(emailNotExist)
-                    $('#user_email').css('border-color','black');
+                    }
+                    else{
+                        $('#user-validation').html(emailNotExist)
+                        $('#user_email').css('border-color','grey');
 
 
+                    }
                 }
         })
         .fail(function() {
