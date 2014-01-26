323693b94b33adff28550f262952b96d8219f058
SandeepK <sinha.sandeep01@gmail.com>
2014-01-26 20:31:17 +0530

    deleted unwanted file

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $progress = $("#progress"), $amount = $("#amount"), panels = [], panels[0] = "panel1", panels[1] = "panel2", panels[3] = "thanks", i = 0, $formPanel = $(".form-panel");
+      //call progress bar constructor
+      $progress.progressbar({
+          change: function() {
+          $amount.text($progress.progressbar("option", "value") + "%");
+      }
+    });
 
     function isAnyFieldEmpty(fields){
         for(i=0;i<fields.length-1;i++){
@@ -7,19 +14,16 @@ $(document).ready(function(){
         return false;
     }
 
-    $('#dob').datepicker({
-        dateFormat: "dd/mm/yy",
-        minDate: (new Date("/7/12/1993")), maxDate: (new Date("/7/12/1996"))
-    });
+//    $('#dob').datepicker();
 
     $('#submit').click(function(){
-        var allFields=[$('#name').val(),$('#email').val(),$('#dob').val(),$('#address').val(),$('#city').val(),$('#number').val()];
-
+        var allFields=[$('#name').val(),$('#email').val(),$('#city').val(),$('#number').val()];
         if(!isAnyFieldEmpty(allFields)) {
             alert("Successfully completed");
         }
 
     })
+
 
     $("#email").change(function(){
         var email= $(this).val();
@@ -29,7 +33,6 @@ $(document).ready(function(){
             data: data
         })
         .success(function(data){
-                alert("ello")
                 emailExist = "<div id=user-validation>This User is already taken </div>";
                 emailNotExist = "<div id=user-validation> <img src='yes.jpeg' width='16' height='10' alt='Edit Entry' /></div>" ;
                 if(email != '')
@@ -51,5 +54,32 @@ $(document).ready(function(){
             alert('Failure');
         })
     });
+
+    $('#next').click(function(e){
+        var allFields=[$('#name').val(),$('#email').val(),$('#city').val(),$('#number').val()];
+        if(!isAnyFieldEmpty(allFields)) {
+            progressBar()
+            e.preventDefault()
+        }
+    })
+
+    function progressBar(){
+            //next or back?
+            var n = ($(this).attr("id") != "back") ? 1 : -1;
+        changepanel(n);
+    }
+
+    function changepanel(n) {
+        //hide current item
+        $($formPanel[0]).fadeOut(500);
+        //selects next item
+        i = 1
+        //hide next item
+        $($formPanel[i]).delay(505).fadeIn();
+
+        (i != 0) ? $("#back").attr("disabled", null) : $("#back").attr("disabled", "disabled");
+        (i != 2) ? $("#next").attr("disabled", null) : $("#next").attr("disabled", "disabled");
+        $progress.progressbar("option", "value", (i * 50) );
+    }
 });
 
