84a033f3026cd34977bc352ed9f5aeec0d7eb7fe
SandeepK <sinha.sandeep01@gmail.com>
2014-01-30 20:20:07 +0530

    Customized user report in admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
-
+  index do
+    column 'Email',:email
+    column 'Name',:name
+    column 'Date Of Birth',:date_of_birth
+    column 'Gender',:gender
+    column 'Address',:address
+    column 'City',:city
+    column 'Contact Number',:mobile_number
+    column 'Passing Year',:year_of_pass
+    column 'Aptitude Center',:preferred_aptitude_center
+    column 'GD Center',:preferred_gd_center
+    default_actions
+  end
 end
