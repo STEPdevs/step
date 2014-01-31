dfd6ea876b130fb9eedfa8ea098994fbb45af8d3
SandeepK <sinha.sandeep01@gmail.com>
2014-01-31 10:30:49 +0530

    Sandeep - Created seed file and search button on admin candidates page

--- app/assets/javascripts/adminPage.js
+++ app/assets/javascripts/adminPage.js
@@ -64,15 +64,3 @@ var adminPage = (function () {
         }
     };
 })();
-
-//
-//    function bindDumpButton() {
-//        $('body').on('click', 'button[name=dump]', function () {
-//            var dump = $(this).data('dump');
-//            var $container = $(dump);
-//            console.log('data of ' + dump, $container.handsontable('getData'));
-//        });
-//    }
-//    bindDumpButton();
-
-//JSON.stringify($('#example').handsontable('getData'))
