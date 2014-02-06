f4c31327bf611c2488367f07fa908db5d534ff2a
SandeepK <sinha.sandeep01@gmail.com>
2014-02-06 18:16:13 +0530

    Sandy/Ravi- testing production

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Step
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += ['jquery.handsontable.full.js', 'jquery-ui-v1.10.4.js','registration.js']
+    config.assets.precompile += ['*.js']
   end
 end
