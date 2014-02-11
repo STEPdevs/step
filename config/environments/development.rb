8578d88cfdb47bec4a7f2a21b41575063d8f2dfb
Anil Kumar Kodi <aniltpl08@gmail.com>
2014-02-11 17:35:42 +0530

    Anil Added smtp configrations and removed address column in activerecord

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,15 +8,17 @@ Step::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
-      :openssl_verify_mode  => 'none',
-      tls: false,
-      enable_starttls_auto: true,#this is the important stuff!
-      :address        => 'smtp.gmail.com',
-      :port           => 587,
-      :domain         => 'step-test.herokuapp.com',
-      :authentication => :plain,
-      :user_name      => 'thoughtwork.step@gmail.com',
-      :password       => 'thoughtworks'
+      #:openssl_verify_mode  => 'none',
+      #tls: false,
+      #enable_starttls_auto: true,#this is the important stuff!
+      #:address        => 'smtp.gmail.com',
+      #:port           => 587,
+      #:domain         => 'step-test.herokuapp.com',
+      #:authentication => :plain,
+      #:user_name      => 'thoughtwork.step@gmail.com',
+      #:password       => 'thoughtworks'
+      :address => "sifymisc01.thoughtworks.com",
+      :domain => "thoughtworks,com"
   }
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
