687fb0635782022a136a9ffe9388a58198018720
Gaurav <gauravmi@thoughtworks.com>
2014-02-14 11:49:05 +0530

    flash message for incoplete information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,16 +33,18 @@ class UsersController < ApplicationController
 		def create_user_with_other_information
 			@user = User.find_by_phone_number(params[:phone_number])
 			if @user
-				@user.other_user_details = OtherUserDetails.new(params[:other_user_details]) unless OtherUserDetails.find_by_users_phone_number(params[:phone_number])
+				@user.other_user_details = OtherUserDetails.new(params[:other_user_details]) unless OtherUserDetails.find_by_users_phone_number(params[:phone_number])				
 				respond_to do |format|
 					if @user.save
 						format.html {redirect_to root_path,:flash=>{success:"registration successful"}}
 		  	  	format.json {render :json=>@user.other_user_details}
 			    else
-						format.html {render action:"index",:flash=>{success:"registration error occured"}}
+						format.html {render action:"index",:flash=>{success:"egistration error occured, please check if all the fields are entered correctcly"}}
 			      format.json {render :json=>@user.other_user_details.errors}
 			    end
 				end
+			else
+				redirect_to root_path,:flash=>{success:"Registration error occured, please check if all the fields are entered correctcly"}
 			end
 		end
 
