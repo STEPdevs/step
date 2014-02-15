caae3cd1727462f5e368e157218f879967ec5b30
Gaurav <gauravmi@thoughtworks.com>
2014-02-15 19:09:54 +0530

    added precompile assets to true

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,14 +2,6 @@ require 'spec_helper'
 
 describe UsersController do
 	describe "create" do
-		it "should create user with valid phone number" do
-			phone_number="9089898989"
-			post :create,format: 'json',user: {phone_number: phone_number}
-			parseResponse = JSON.parse(response.body)
-			expect(response.header['Content-Type'].include?('application/json')).to be_true
-			expect(parseResponse["phone_number"]).to eq(phone_number)
-		end
-
 		{"123456789"=>"is too short","12341234#0"=>"is too short","qwabcdefgh"=>"is too short","12345678bc"=>"is too short"}.each do |invalid_number,msg|
 			it "should not create user for invalid number" do
 				post :create,format: 'json',user: {phone_number: "12341234"}
@@ -20,7 +12,15 @@ describe UsersController do
 			end
 		end
 
-		it "should create user for valid phonenumber" do
+		it "should create user with valid phone number" do
+			phone_number="9089898989"
+			post :create,format: 'json',user: {phone_number: phone_number}
+			parseResponse = JSON.parse(response.body)
+			expect(response.header['Content-Type'].include?('application/json')).to be_true
+			expect(parseResponse["phone_number"]).to eq(phone_number)
+		end
+
+		it "should create user and fill other user details for valid phonenumber" do
 			phone_number="9089898989"
 			post :create,format: 'json',user: {phone_number: phone_number}
 			parseResponse = JSON.parse(response.body)
@@ -34,7 +34,7 @@ describe UsersController do
 			expect(parseResponse["name"]).to eq("name1")
 		end
 
-		it "should not create user if both the user and information for the user is present" do
+		it "should not create user if all the user information for the user is present" do
 			phone_number="9089898989"
 			post :create,format: 'json',user: {phone_number: phone_number}
 			parseResponse = JSON.parse(response.body)
