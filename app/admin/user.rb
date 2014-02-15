5b640838c1e3678a2a28abdd17a2088d3e0f7715
Gaurav <gauravmi@thoughtworks.com>
2014-02-15 18:57:21 +0530

    columns added to incomplete users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,6 +2,14 @@ ActiveAdmin.register User do
 	menu :label =>"Incomplete profiles"
 	config.sort_order = "count_desc"
 	config.filters = false
+	index do
+		column :phone_number
+		column :count
+		column :status
+		column :created_at
+		column :updated_at
+		default_actions
+	end
 
 	controller do
 	  def scoped_collection
