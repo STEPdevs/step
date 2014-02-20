799a308b90832d3bee04200099ad0fa1c2306465
vikytech <famous.vikhyath@gmail.com>
2014-02-20 19:18:00 +0530

    [Viky] Added a timeout for menu and gave loading indicator as well and Refactored the same

--- app/assets/javascripts/modify.js
+++ app/assets/javascripts/modify.js
@@ -1,13 +1,29 @@
 function modifyPage(url) {
-            $.ajax({
-                type: "GET",
-                url: url,
-                cache: false,
-                success: function (html) {
-                    $('.content').empty();
-                    $('.content').append(html);
-                    $( "div#header")[1].remove();
-                }
-            });
-    return true;
+    var loadingIndicator = $('.loading');
+    loadingIndicator.show();
+    $.ajax({
+            type: "GET",
+            url: url,
+            cache: false,
+            error: function (xhr, status, error) {
+                var content = $('.content');
+                content.empty();
+                content.append(loadingIndicator.show());
+                setTimeout(function () {
+                    var networkDownError = "<pre><b>                                              Network Error !! Please try again later</b></pre>";
+                    content.append((xhr.responseText == null || xhr.responseText == "")
+                        ? networkDownError
+                        : xhr.responseText);
+                    loadingIndicator.hide();
+                }, 3000);
+            },
+            success: function (html) {
+                var content = $('.content');
+                content.hide().empty().append(html).append(loadingIndicator.hide());
+                var contentHeader = $('div#header')[1];
+                contentHeader.remove();
+                content.show();
+            }
+        }
+    );
 }
